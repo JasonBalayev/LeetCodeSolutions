dea18c9fd53cd59f7efe9973fddbacee987231ff
Jason <jasonbalayev@gmail.com>
2024-08-24 15:59:12 -0400

    Initial commit with LeetCode solutions

---
+++ TwoSum.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        paired_index= {}
+        for i, num in enumerate(nums):
+            if target - num in paired_index:
+                return [i, paired_index[target-num]]
+            paired_index[num] = i
