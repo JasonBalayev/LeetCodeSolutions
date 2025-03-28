288bdf7f1be850c4f00375fda012a293aa7af5c7
Jason <jasonbalayev@gmail.com>
2025-03-28 13:16:57 -0400

    p69 easy solved

---
+++ Easy/Sqrt(x)/Sqrt(x).py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        
+        left, right = 1, x
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right
+    
+#QED
+#Problem 69 (Easy Of Sqrt(x)) - Jason Balayev (python)
