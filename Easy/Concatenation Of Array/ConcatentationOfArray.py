991a2b94a68d8eb34c2f75286e470a00162c4fa8
Jason <jasonbalayev@gmail.com>
2025-04-04 17:58:48 -0400

    p1929 solved easy

---
+++ Easy/Concatenation Of Array/ConcatentationOfArray.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+        
+#QED
+#Problem 1929 (Easy Of Concatenation Of Array) - Jason Balayev (python)
