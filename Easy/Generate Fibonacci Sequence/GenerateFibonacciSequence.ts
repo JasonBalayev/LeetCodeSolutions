a00abef7bbf3973c25eb7978c0dd09e9f7756523
Jason <jasonbalayev@gmail.com>
2025-04-08 16:34:54 -0400

    p2648 solved easy

---
+++ Easy/Generate Fibonacci Sequence/GenerateFibonacciSequence.ts
@@ -0,0 +1,16 @@
+function* fibGenerator(): Generator<number, any, number> {
+    let a = 0, b = 1;
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+    }
+};
+
+//QED
+//Problem 2648 (Easy Of Generate Fibonacci Sequence) - Jason Balayev (typescript)
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
