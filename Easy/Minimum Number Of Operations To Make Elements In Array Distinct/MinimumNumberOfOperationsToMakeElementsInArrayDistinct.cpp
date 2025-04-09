83bf7dfeff650da2f5f90756e7ea21104e5728d5
Jason <jasonbalayev@gmail.com>
2025-04-09 01:53:00 -0400

    p3396 solved easy

---
+++ Easy/Minimum Number Of Operations To Make Elements In Array Distinct/MinimumNumberOfOperationsToMakeElementsInArrayDistinct.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int op = 0;
+        vector<int> remaining = nums;
+        
+        while (!remaining.empty()) {
+            unordered_set<int> uniqueElements(remaining.begin(), remaining.end());
+            if (uniqueElements.size() == remaining.size()) {
+                return op; 
+            }
+            int elementsToRemove = min(3, static_cast<int>(remaining.size()));
+            remaining.erase(remaining.begin(), remaining.begin() + elementsToRemove);
+            op++;
+        }
+        return op;
+    }       
+};
+
+//QED
+//Problem 3396 (Easy of Minimum Number of Operations to Make Elements in Array Distinct) - Jason Balayev (python)
