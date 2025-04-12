ed6ecac45fe5ccd68f32cf5e3271bdd640804c77
Jason <jasonbalayev@gmail.com>
2025-04-12 14:05:30 -0400

    p3512 solved easy

---
+++ Easy/Minimum Operations To Make Array Sum Divisible By K/MinimumOperationsToMakeArraySumDivisibleByK.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        total_sum = sum(nums)
+        remainder=total_sum % k
+        
+        if remainder==0:
+            return 0
+        return remainder
+    
+#QED
+#Problem 2878 (Easy of Minimum Operations to Make Array Sum Divisible by K) - Jason Balayev (python)
