30dced608ce8851daaf0656a0d637f41f227fa59
Jason <jasonbalayev@gmail.com>
2025-04-15 14:31:27 -0400

    p392 solved easy

---
+++ LeetCode 75/Is Subsequence/IsSubsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length()==0) {
+            return true;
+        }
+        int sIdx =0;
+        int tIdx =0;
+        while (tIdx<t.length()) {
+            if(s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+                if(sIdx == s.length()) {
+                    return true;
+                }
+            }
+            tIdx++;
+        }
+        return false;
+    }
+}
+
+//QED
+//Problem 392 (Easy of Is Subsequence) - Jason Balayev (java)
