78933214df8066ef40a4bbdd6ae8e3df23485422
Jason <jasonbalayev@gmail.com>
2025-04-17 18:17:34 -0400

    p1004 solved med

---
+++ LeetCode 75/Max Consecutive Ones III/MaxConsecutiveOnesIII.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        l=0
+        max_len=0
+        zeros=0
+
+        for r in range(len(nums)):
+            if nums[r]==0:
+                zeros+=1
+            while zeros>k:
+                if nums[l]==0:
+                    zeros-=1
+                l+=1
+            max_len=max(max_len,r-l+1)
+        return max_len
+        
+#QED
+#Problem 1004 (Medium of Max Consecutive Ones III) - Jason Balayev (python)
