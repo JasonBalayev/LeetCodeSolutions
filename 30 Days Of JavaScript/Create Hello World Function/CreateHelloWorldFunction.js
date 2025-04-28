a319b0d40c539c52891457645bbb80e9a1b803a2
Jason <jasonbalayev@gmail.com>
2025-04-28 10:37:40 -0400

    p2267 solved easy

---
+++ 30 Days Of JavaScript/Create Hello World Function/CreateHelloWorldFunction.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+//QED
+//Problem 2667 (Easy Of Create Hello World Function) - Jason Balayev (javascript)
