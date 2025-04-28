5a10759452f3f9a9bf4ca42498a37129c469c014
Jason <jasonbalayev@gmail.com>
2025-04-28 10:40:28 -0400

    p2620 solved easy

---
+++ 30 Days Of JavaScript/Counter/Counter.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+
+//QED
+//Problem 2620 (Easy Of Counter) - Jason Balayev (javascript)
