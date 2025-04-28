ad80dcfbaec2c2814bf1f02496a2ee91b13a68f6
Jason <jasonbalayev@gmail.com>
2025-04-28 10:50:33 -0400

    p2704 solved easy

---
+++ 30 Days Of JavaScript/To Be Or Not To Be/ToBeOrNotToBe.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(val2){
+            if (val!==val2){
+                throw new Error("Not Equal");
+            }
+            return true;
+        },
+        notToBe: function(val2){
+            if (val===val2){
+                throw new Error("Equal");
+            }
+            return true;
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
+
+//QED
+//Problem 2705 (Easy Of To Be Or Not To Be) - Jason Balayev (javascript)
