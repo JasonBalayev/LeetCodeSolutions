6aba90697aebfe875e366179beabda29feea3b09
Jason <jasonbalayev@gmail.com>
2025-05-01 11:30:18 -0400

    p2629 solved easy

---
+++ 30 Days Of JavaScript/Function Composition/FunctionComposition.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+function compose(functions) {
+    if (functions.length === 0) {
+        return function(x) {
+            return x;
+        };
+    }
+    
+    return function(x) {
+        let result = x;
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+        return result;
+    };
+}
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+
+//QED
+//Problem 2629 (Easy Of Function Composition) - Jason Balayev (javascript)
