3aa24d263f2345b270d3d2729ee995427d5959c8
Jason <jasonbalayev@gmail.com>
2025-05-01 11:35:05 -0400

    p2703 solved easy

---
+++ 30 Days Of JavaScript/Return Length Of Arguments Passed/ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,14 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+
+//QED
+//Problem 2703 (Easy Return Length of Arguments Passed) - JavaScript
