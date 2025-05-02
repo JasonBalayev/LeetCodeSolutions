e3cda2544b55f6fc076e26a2cda30767605b9d66
Jason <jasonbalayev@gmail.com>
2025-05-02 13:14:59 -0400

    p2727 solved easy

---
+++ 30 Days Of JavaScript/Is Object Empty/IsObjectEmpty.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    return Object.keys(obj).length === 0;
+};
+
+//QED
+//Problem 2727 (Easy Of Is Object Empty) - Jason Balayev (javascript)
