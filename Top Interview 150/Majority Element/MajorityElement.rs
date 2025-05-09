a53ab2d0517b259be43111547f0fb12aa2e88ac0
Jason <jasonbalayev@gmail.com>
2025-05-09 16:51:27 -0400

    top150 started

---
+++ Top Interview 150/Majority Element/MajorityElement.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn majority_element(nums: Vec<i32>) -> i32 {
+        let mut count=0;
+        let mut element=0;
+        for num in nums{
+            if count==0{
+                element=nums;
+            }
+            count+=if num==element{1}else{-1};
+        }
+        element
+    }
+}
+
+//QED
+//Problem 169 (Easy) - Majority Element - Jason Balayev (Rust)
