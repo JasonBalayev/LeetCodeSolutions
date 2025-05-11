b77599993a853746a3246c82490cc295a93494d4
Jason <jasonbalayev@gmail.com>
2025-05-11 01:05:59 -0400

    p123 solved hard

---
+++ Top Interview 150/Best Time To Buy And Sell Stock III/BestTimeToBuyAndSellStockIII.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        if prices.len()<2{
+            return 0;
+        }
+        let mut buy1=-prices[0];
+        let mut sell1=0;
+        let mut buy2=-prices[0];
+        let mut sell2=0;
+        for i in 1..prices.len(){
+            buy1=buy1.max(-prices[i]);
+            sell1=sell1.max(buy1+prices[i]);
+            buy2=buy2.max(sell1-prices[i]);
+            sell2=sell2.max(buy2+prices[i]);
+        }
+        sell2
+    }
+}
+
+//QED
+//Problem 123 (Hard of Best Time to Buy and Sell Stock III) - Jason Balayev (rust)
