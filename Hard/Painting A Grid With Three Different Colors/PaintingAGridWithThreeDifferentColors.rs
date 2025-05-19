de5d241db4dc642a1a303f56bb9b3528d1d4b9c5
Jason <jasonbalayev@gmail.com>
2025-05-18 22:29:59 -0400

    p1931 solved hard

--- Hard/Painting A Grid With Three Different Colors/PaintingAGridWithThreeDifferentColors.rs
+++ Hard/Painting A Grid With Three Different Colors/PaintingAGridWithThreeDifferentColors.rs
@@ -1,81 +1,56 @@
 impl Solution {
     pub fn color_the_grid(m: i32, n: i32) -> i32 {
+        const Mod:i32=1_000_000_007;
         let m=m as usize;
         let n=n as usize;
-        const Mod:i32=1_000_000_007;
-        if m==1{
-            let mut res:i64=3;
-            for i in 1..n{
-                res=(res*2)%(Mod as i64);
+        let total_states = 3usize.pow(m as u32);
+        let mut valid_states=Vec::new();
+        let mut color=vec![vec![0; m];total_states];
+        let mut dp=vec![vec![0; total_states];n+1];
+        let mut is_valid=vec![vec![0;total_states];total_states];
+        for i in 0..total_states{
+            let mut val=i;
+            let mut is_ok=true;
+            for j in 0..m{
+                color[i][j]=val%3;
+                val/=3;
             }
-            return res as i32;
-        }
-        if m==2{
-            if n==1{
-                return 6;
-            }
-            if n==2 {
-                return 18;
-            }
-            let mut a:i64=6;
-            let mut b:i64=6;
-            for i in 1..n{
-                let new_a=(3*a+2*b)%(Mod as i64);
-                let new_b=(2*a+2*b)%(Mod as i64);
-                a=new_a;
-                b=new_b;
-            }
-            return a as i32;
-        }
-        let mut states=Vec::new();
-        let mut map=std::collections::HashMap::new();
-        fn gen_states(row:usize,m:usize,curr:Vec<u8>,states:&mut Vec<Vec<u8>>){
-            if row==m{
-                states.push(curr);
-                return;
-            }
-            for color in 1..=3{
-                if row>0&&curr[row-1]==color{
-                    continue;
+            for j in 1..m{
+                if color[i][j]==color[i][j-1]{
+                    is_ok=false;
+                    break;
                 }
-                let mut new_curr=curr.clone();
-                new_curr.push(color);
-                gen_states(row+1,m,new_curr,states);
+            }
+            if is_ok{
+                valid_states.push(i);
+                dp[1][i]=1;
             }
         }
-        
-        gen_states(0,m,Vec::new(),&mut states);
-        
-        for (i,state) in states.iter().enumerate(){
-            map.insert(state.clone(),i);
-        }
-        let count=states.len();
-        let mut adj=vec![Vec::new();count];
-        for i in 0..count{
-            for j in 0..count{
-                let mut valid=true;
+        for &i in &valid_states{
+            for &j in &valid_states{
+                is_valid[i][j]=1;
                 for k in 0..m{
-                    if states[i][k]==states[j][k]{
-                        valid=false;
+                    if color[i][k]==color[j][k]{
+                        is_valid[i][j]=0;
                         break;
                     }
                 }
-                if valid{
-                    adj[i].push(j);
-                }
             }
         }
-        let mut dp=vec![1i64;count];
-        for _ in 1..n{
-            let mut new_dp=vec![0i64;count];
-            for i in 0..count{
-                for &j in &adj[i]{
-                    new_dp[j]=(new_dp[j]+dp[i])%(Mod as i64);
+        for col in 2..=n{
+            for &i in &valid_states{
+                for &j in &valid_states{
+                    if is_valid[i][j]==1{
+                        dp[col][i]=(dp[col][i]+dp[col-1][j])%Mod;
+                    }
                 }
             }
-            dp=new_dp;
+        }        
+        let mut res=0;
+        for &i in &valid_states{
+            res=(res+dp[n][i])%Mod;
         }
-        (dp.iter().sum::<i64>()%(Mod as i64)) as i32
+        res
     }
 }
 
