611dea836789d986c999449da7ebe300b491d505
Jason <jasonbalayev@gmail.com>
2025-07-12 21:49:55 -0400

    p2410 solved med

---
+++ Medium/Maximum Matching Of Players With Trainers/MaximumMatchingOfPlayersWithTrainers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+        int n=players.size();
+        int m=trainers.size();
+        int i=0,j=0,ans=0;
+        while(i<n&&j<m){
+            if(players[i]<=trainers[j]){
+                ans++;
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return ans;
+    }
+};
+
+//QED
+//Problem 2410 (Medium of Maximum Matching Of Players With Trainers) - Jason Balayev (cpp)
