8222292567344dd7d10da9f4ae800151170bbbcd
J <156387759+JasonBalayev@users.noreply.github.com>
2026-01-11 23:12:47 -0500

    p1266 solved easy

---
+++ Easy/Minimum Time Visiting All Points/MinimumTimeVisitingAllPoints.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        total_time=0
+        for i in range(1,len(points)):
+            dx=abs(points[i][0]-points[i-1][0])
+            dy=abs(points[i][1]-points[i-1][1])
+            total_time+=max(dx,dy)
+        return total_time
+
+#QED
+#Problem 1266 (Easy of Minimum Time Visiting All Points) - Jason Balayev (python)
